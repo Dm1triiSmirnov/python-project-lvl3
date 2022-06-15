6bfa9e3a76977c929ee53e064769d360a9b69f2d
Dm1triiSmirnov <smirnov0719@gmail.com>
2022-06-15 15:21:58 +0300

    step 5: add logger messages and sys.exit() in to the page_loader

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,7 +1,13 @@
+import logging
 from page_loader.url_converter import (convert_output_file_name,
                                        convert_file_path,
                                        convert_output_dir_name)
 from page_loader.content_loader import download_all_content
+from page_loader.logger import configurate_logger
+
+
+logging.getLogger(__name__)
+configurate_logger()
 
 
 def download(output_directory, url):
@@ -12,4 +18,5 @@ def download(output_directory, url):
     html_doc = download_all_content(url, output_files_path)
     with open(output_path, 'w') as file:
         file.write(html_doc)
+    logging.info('Web page including all available content successfully downloaded')  # noqa E501
     return output_path
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,11 +1,26 @@
+import logging
+import sys
+
 from page_loader.args_parser import args_parse
 from page_loader.page_loader import download
+from page_loader.logger import configurate_logger
+
+
+logging.getLogger(__name__)
+configurate_logger()
 
 
 def main():
     args = args_parse()
-    file_path = download(args.output, args.url)
-    print(f'Web page successfully downloaded as: {file_path}')
+    try:
+        file_path = download(args.output, args.url)
+    except Exception as error:
+        logging.exception('Exception occurred', exc_info=True)
+        print(f'Exception occurred: {error}')
+        sys.exit()
+    else:
+        print(f'Web page successfully downloaded as: {file_path}')
+    sys.exit()
 
 
 if __name__ == '__main__':
